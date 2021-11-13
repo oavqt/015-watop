e86671b276e4df258c4e97cfe035645a20ac9f90
oav <ortizqtv@gmail.com>
2021-11-12 22:08:22 -0600

    Create a function that processes data from the API

--- src/weather.js
+++ src/weather.js
@@ -1,4 +1,4 @@
-async function fetchWeather(location, unit = 'metric') {
+async function fetchWeather(location, unit) {
   const publicKey = 'c3d8c66c21f63dca9b58e9ae48dcf602';
 
   try {
@@ -7,8 +7,43 @@ async function fetchWeather(location, unit = 'metric') {
     );
     const weather = await response.json();
 
+    console.log(weather);
     return weather;
   } catch (err) {
     throw new Error(err);
   }
 }
+
+function processWeather(data) {
+  return {
+    weather: {
+      current: data.weather[0].main,
+      description: data.weather[0].description
+    },
+    temperature: {
+      number: {
+        current: data.main.temp,
+        feels: data.main.feels_like,
+        humidity: data.main.humidity,
+        max: data.main.temp_max,
+        min: data.main.temp_min,
+        pressure: data.main.pressure
+      }
+    },
+    wind: {
+      speed: data.wind.speed,
+      deg: data.wind.deg
+    }
+  };
+}
+
+// function displayWeather(weather) {}
+
+async function getWeather(location, unit = 'metric') {
+  const fetch = await fetchWeather(location, unit);
+  const process = processWeather(fetch);
+
+  console.log(process);
+}
+
+getWeather('temple');
